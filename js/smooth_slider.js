a82a2a32aec947bcf129d10975e520c738a17081
ardipta <ashiqur35-2149@diu.edu.bd>
2021-02-15 15:18:54 +0600

    Finilize template

---
+++ js/smooth_slider.js
@@ -0,0 +1,20 @@
+$("#aboutClick").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+});
+$("#wordsCLick").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#words").offset().top
+    }, 1000);
+});
+$("#worksClick").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#works").offset().top
+    }, 1000);
+});
+$("#contactClick").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+});
